e7c9ae539208d5f6569ee42df83e43504fa77333
Roshaad Johnson <roshaadjohnson@gmail.com>
2021-10-13 18:55:20 -0500

    assignment

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-function generatePassword(){
-var includeLowerCase = confirm('do you want to confirm lowercase')
-var password
-var passwordLength = alert('how long is your password')
-console.log(passwordLength)
-if (includeLowerCase) {
-  password = 'user wants lowerCase'
-}
-  else password = 'user doesnt want lowerCase'
 
-  return password;
+function generatePassword(){
+var includeLowerCase = confirm('do you want to include lowercase characters?')
+var includeUppperCase = confirm('do you want to include uppercase characters?')
+var includeNumberic = confirm('do you want to include numeric characters?')
+var includeSpecialChar = confirm('do you want to include special characters?')
+var passwordLength = window.prompt('How long is your password? Must be between 8 and 128 characters')
+console.log(passwordLength)
+
+var password = '';
+for (var i = 0; i < passwordLength; i++) {
+  password += String.fromCharCode(Math.floor(Math.random() * 77) + 34);
+  if (includeLowerCase){
+    password.toLowerCase();
+  }
+  if (!includeUppperCase){
+    password.toLowerCase();
+  }
+
 }
+return password;
+ 
+}
+
+
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+return password;  
 
 }
 
